060beba13c95c27fd01fdca94d0ee30619710611
Stefan Baggoo <itsrhyett@gmail.com>
2023-04-02 10:22:56 -0400

    Added scores increase and function to deal with setting a game limit.

--- script.js
+++ script.js
@@ -3,12 +3,17 @@ const rockButton = document.getElementById('rock');
 const paperButton = document.getElementById('paper');
 const scissorsButton = document.getElementById('scissors');
 const choice = ['rock', 'paper', 'scissors'];
-let computerDecsion;
+
 //score keepers
 let playerScore = 0;
 let computerScore = 0;
+let gamesPlayed = 0;
+let gameLimit = 0;
 
-//computer's decision
+//set game limit
+function setGameLimit(element){
+    gameLimit = element;
+}
 
 //function to compare results
 function compare(playerChoice){
@@ -16,38 +21,40 @@ function compare(playerChoice){
     let computerDecision = Math.floor(Math.random() * choice.length);
 
     if(playerChoice === choice[computerDecision]){
-        console.log('tie Game!')
+        gamesPlayed++;
     }
     //if player's selection is rock and computer's selection is paper, computer wins
     else if(playerChoice === choice[0] && choice[computerDecision] === choice[1]){
-        console.log('Computer Wins!');
+        gamesPlayed++;
+        computerScore++;
     }
     //if player's selection is rock and computer's selection is scissors, player wins
     else if(playerChoice === choice[0] && choice[computerDecision] === choice[2]){
-        console.log('player wins!');
+        gamesPlayed++;
+        playerScore++;
     }
-    //if player's selection is paper and computer selection is rock, player winse
+    //if player's selection is paper and computer selection is rock, player wins
     else if(playerChoice === choice[1] && choice[computerDecision] === choice[0]){
-        console.log('player wins')
+        gamesPlayed++;
+        playerScore++;
     }
     //if player's selection is paper and computer selection is scissors, computer wins
     else if(playerChoice === choice[1] && choice[computerDecision] === choice[2]){
-        console.log('computer wins!')
+        gamesPlayed++;
+        computerScore++;
     }
     //if player's selection is scissors and computer selection is rock, computer wins
     else if(playerChoice === choice[2] && choice[computerDecision] === choice[0]){
-        console.log('computer wins')
+        gamesPlayed++;
+        computerScore++;
     }
     //if player's selection is scissors and computer selection is paper, player wins
     else if(playerChoice === choice[2] && choice[computerDecision] === choice[1]){
-        console.log('player wins!');
+        gamesPlayed++;
+        playerScore++;
     }
-    else{
-        console.log('error!', playerChoice, choice[computerDecision]);
-
-    }
-    
-        
+    else
+        console.log('error!', playerChoice, choice[computerDecision]); 
 };
 
 //user selectection
